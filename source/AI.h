f13f8859821f07ab696949ae138432da74bb8a83
tehhowch <tehhowch@users.noreply.github.com>
2020-01-19 11:32:46 -0600

    Update AI.h

--- source/AI.h
+++ source/AI.h
@@ -9,6 +9,7 @@ Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 */
+// Adhoc change
 
 #ifndef AI_H_
 #define AI_H_
