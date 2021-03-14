4d2fd27cd0a83a23b311391c9398feca98dca8fe
tehhowch <tehhowch@users.noreply.github.com>
2021-03-14 13:27:13 -0500

    feat(internal): create a UUID class to manage serializable identifiers
     - lazy value initialization
     - move-only (copying a UUID isn't permitted, as that means there are two separate things with the same unique identifier)
       - could allow copying, but treat that as a "cleared the value" scenario, such that the new ID will have a different value than the copied-from ID. (This would allow owning objects to be copied, but would also change the semantics to "it's a doppelganger, not a copy.")

---
+++ source/Uuid.h
@@ -0,0 +1,58 @@
+/* Uuid.h
+Copyright (c) 2021 by Benjamin Hauch
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#ifndef ES_UUID_H_
+#define ES_UUID_H_
+
+#include <string>
+
+
+
+// Class wrapping IETF v4 GUIDs, providing lazy initialization.
+class Uuid final {
+public:
+	static Uuid FromString(const std::string &input);
+	Uuid() noexcept = default;
+	~Uuid() noexcept = default;
+	// UUIDs cannot be copied.
+	Uuid(const Uuid &other) = delete;
+	Uuid &operator=(const Uuid &other) = delete;
+	// UUIDs can be moved.
+	Uuid(Uuid &&other) noexcept = default;
+	Uuid &operator=(Uuid &&other) noexcept = default;
+	
+	// UUIDs can be compared against other UUIDs.
+	bool operator==(const Uuid &other) const;
+	bool operator!=(const Uuid &other) const;
+	
+	// Get a string representation of this ID, e.g. for serialization.
+	const std::string &ToString() const;
+	
+	
+private:
+	// Internal constructor, from a string.
+	explicit Uuid(const std::string &input);
+	// Lazy initialization getter.
+	const std::string &Value() const;
+	
+	
+private:
+	// The internal representation of the UUID. For now, we store the UUID as an
+	// arbitrary-length string, rather than the more correct collection of bytes.
+	mutable std::string value;
+	
+	
+};
+
+
+
+#endif
